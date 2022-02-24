c784ba41220c4ecc151bd64a77c2b3e8af597add
chriskorsak <chris@chriskorsak.com>
2022-02-24 00:20:41 -0800

    Create feedback data.

---
+++ src/data/feedbackData.js
@@ -0,0 +1,19 @@
+const feedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo, rem modi nemo tenetur voluptas tempore repellat, dolore mollitia iste omnis sunt. Ipsa accusamus modi obcaecati nam debitis architecto numquam quidem.',
+  },
+  {
+    id: 2,
+    rating: 7,
+    text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo, rem modi nemo tenetur voluptas tempore repellat, dolore mollitia iste omnis sunt. Ipsa accusamus modi obcaecati nam debitis architecto numquam quidem.',
+  },
+  {
+    id: 3,
+    rating: 4,
+    text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo, rem modi nemo tenetur voluptas tempore repellat, dolore mollitia iste omnis sunt. Ipsa accusamus modi obcaecati nam debitis architecto numquam quidem.',
+  },
+];
+
+export default feedbackData;
