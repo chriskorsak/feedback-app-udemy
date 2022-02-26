4123c720e975e489bb87882a12bb3ff090e5fae5
chriskorsak <chris@chriskorsak.com>
2022-02-25 16:15:24 -0800

    create feedback stats component and implement in app.

---
+++ src/components/FeedbackStats.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+
+const FeedbackStats = ({ feedback }) => {
+  //calculate feedback rating average
+  let average =
+    feedback.reduce((total, current) => total + current.rating, 0) /
+    feedback.length;
+
+  //only allow 1 decimal place & remove with regex if int
+  average = average.toFixed(1).replace(/[.,]0$/, '');
+
+  return (
+    <div className="feedback-stats">
+      <h4>
+        {feedback.length} {feedback.length === 1 ? 'Review' : 'Reviews'}
+      </h4>
+      <h4>Average Rating: {isNaN(average) ? 0 : average} </h4>
+    </div>
+  );
+};
+
+FeedbackStats.propTypes = {
+  feedback: PropTypes.array.isRequired,
+};
+
+export default FeedbackStats;
